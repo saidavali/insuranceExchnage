3ffe2813c67e6361ee6c9d6e52ae25881f91d409
saidavali <saidavali12@gmail.com>
2021-07-21 18:36:18 +0530

    RegisterNow added

---
+++ client/src/app/insurance-exchange-register-user/insurance-exchange-register-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-insurance-exchange-register-user',
+  templateUrl: './insurance-exchange-register-user.component.html'
+})
+export class InsuranceExchangeRegisterUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
